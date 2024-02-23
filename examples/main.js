ff5fcae929f133417e51e966c6ce31a415b02493
gaohao <hao.gao@vixtel.com>
2024-02-23 18:02:38 +0800

    feat: 添加markdown预览组件

--- examples/main.js
+++ examples/main.js
@@ -8,10 +8,21 @@ import "ant-design-vue/dist/antd.css";
 // 引入组件
 import MyUi from '../src/index'
 
+// v-md-editor markdown
+import VueMarkdownEditor from '@kangc/v-md-editor';
+import '@kangc/v-md-editor/lib/style/base-editor.css';
+import vuepressTheme from '@kangc/v-md-editor/lib/theme/vuepress.js';
+import '@kangc/v-md-editor/lib/theme/style/vuepress.css';
+import Prism from 'prismjs';
+
+VueMarkdownEditor.use(vuepressTheme, {
+    Prism,
+});
+
 const app = createApp(App);
 app.use(store);
 app.use(router);
 app.use(Antd);
 app.use(MyUi);
-
+app.use(VueMarkdownEditor);
 app.mount('#app');
--- examples/views/security/index.vue
+++ examples/views/security/index.vue
@@ -1,19 +1,20 @@
 <template>
 <div class="page-content">
-    <a-collapse v-model:activeKey="activeKey" accordion destroyInactivePanel>
-        <a-collapse-panel v-for="(item,index) in componentsList" :key="index">
-            <template #header>
-                <div class="panel-content">
-                    <span class="panel-label"> {{ item.name }}</span>
-                    <template v-if="item.description">
-                        <span class="divider">—————</span>
-                        <span class="description">{{ item.description }}</span>
-                    </template>
-                </div>
-            </template>
-            <component :is="item.content"/>
-        </a-collapse-panel>
-    </a-collapse>
+<!--    <a-collapse v-model:activeKey="activeKey" accordion destroyInactivePanel>-->
+<!--        <a-collapse-panel v-for="(item,index) in componentsList" :key="index">-->
+<!--            <template #header>-->
+<!--                <div class="panel-content">-->
+<!--                    <span class="panel-label"> {{ item.name }}</span>-->
+<!--                    <template v-if="item.description">-->
+<!--                        <span class="divider">—————</span>-->
+<!--                        <span class="description">{{ item.description }}</span>-->
+<!--                    </template>-->
+<!--                </div>-->
+<!--            </template>-->
+<!--            <component :is="item.content"/>-->
+<!--        </a-collapse-panel>-->
+<!--    </a-collapse>-->
+    <v-md-editor v-model="text" height="100%"></v-md-editor>
 </div>
 </template>
 
@@ -21,8 +22,30 @@
 import components from "src"
 import {onMounted, reactive, ref} from "vue";
 
-const activeKey = ref([]);
+// const activeKey = ref([]);
 const componentsList = reactive([]);
+const text = ref(`## tip
+
+:::tip
+这是一个 tip。这是一个 tip。这是一个 tip。这是一个 tip。这是一个 tip。这是一个 tip。
+:::
+
+## warning
+
+:::warning
+这是一个 warning，**这是一个 warning，**。
+这是一个 warning。
+这是一个 warning，这是一个 warning，这是一个 warning，这是一个 warning。
+:::
+
+## demo
+
+demo 这是一个 demo。这是一个 demo。这是一个 demo。这是一个 demo。这是一个 demo。这是一个 demo。这是一个 demo。这是一个 demo。
+
+\`\`\`html
+<sq-button></sq-button>
+\`\`\`
+`)
 onMounted(() => {
     for (let key in components) {
         const component = components[key];
